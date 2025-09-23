3669bbf4d11408727fc07d92bf05863c7201bd91
JeongwooShin <49263018+JeongwooShin@users.noreply.github.com>
2025-09-23 19:38:34 +0900

    Update supabaseClients.ts

--- supabase/functions/_shared/supabaseClients.ts
+++ supabase/functions/_shared/supabaseClients.ts
@@ -1,9 +1,9 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2.47.5";
 
 // Edge Functions 환경변수
-const SUPABASE_URL = Deno.env.get("SUPABASE_URL")!;
-const SUPABASE_ANON_KEY = Deno.env.get("SUPABASE_ANON_KEY")!;
-const SUPABASE_SERVICE_ROLE_KEY = Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!;
+const SUPABASE_URL = Deno.env.get("SB_URL")!;
+const SUPABASE_ANON_KEY = Deno.env.get("SB_ANON_KEY")!;
+const SUPABASE_SERVICE_ROLE_KEY = Deno.env.get("SB_SERVICE_KEY")!;
 
 if (!SUPABASE_URL || !SUPABASE_ANON_KEY || !SUPABASE_SERVICE_ROLE_KEY) {
   console.error("Missing Supabase env vars");
